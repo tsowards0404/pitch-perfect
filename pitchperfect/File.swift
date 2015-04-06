2fdba4e97ad6c9b4d7bfb3d1f829cf035084b9e8
tsowards0404 <tsowards0404@gmail.com>
2015-04-06 16:11:29 -0400

    Committed by Udacity

---
+++ pitchperfect/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  pitchperfect
+//
+//  Created by Troy Sowards on 3/13/15.
+//  Copyright (c) 2015 Troy Sowards. All rights reserved.
+//
+
+import Foundation
+
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
